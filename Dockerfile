12502172df54f3c0363a90845c9b10f4ba77db3d
marcocastagnaro <marco.castagnaro@ing.austral.edu.ar>
2025-07-17 14:33:20 -0300

    add validations

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
-# Stage 1: build with Gradle (Debian-based)
 FROM gradle:jdk17 AS build
 WORKDIR /app
 
-# copy your source and build the fat JAR
 COPY --chown=gradle:gradle . /app
 RUN gradle clean bootJar --no-daemon
 
-# Stage 2: runtime on slim Debian
 FROM openjdk:17-slim AS runtime
 WORKDIR /app
 
-# grab the built JAR from the build stage
 COPY --from=build /app/build/libs/*.jar ./app.jar
 
 EXPOSE 8080
--- src/main/kotlin/com/interview/patient_service/service/PatientService.kt
+++ src/main/kotlin/com/interview/patient_service/service/PatientService.kt
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import java.util.regex.Pattern
 
 
 @Service
@@ -17,7 +18,20 @@ class PatientService {
     private lateinit var patientRepository: PatientRepository
 @Autowired
 private lateinit var mailSenderService: MailService
+    private val emailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
+    private val phonePattern = Pattern.compile("^\\+\\d{1,3}\\s?\\d{4,14}\$")
+
     fun createPatient(dto: CreatePatientDto): CreatePatientResponseDto {
+        if (!emailPattern.matcher(dto.email).matches()) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email format")
+        }
+
+        if (!phonePattern.matcher(dto.phone).matches()) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Invalid phone format. Expected +<countrycode> <number>"
+            )
+        }
         if (patientRepository.findByEmail(dto.email).isPresent) {
             throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already registered")
         }
